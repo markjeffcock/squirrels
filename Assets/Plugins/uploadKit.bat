3dd67a17c62b7815200b71b8e147809b006de12c
markjeffcock <mark.jeffcock@gmail.com>
2020-06-15 17:20:00 +0100

    Squirrel on all fours (static)
    Squirrel on all fours (static)

---
+++ Assets/Plugins/uploadKit.bat
@@ -0,0 +1,2 @@
+echo off
+curl -v -b cookie -X PUT -F "kit[zip]=@D:\Google Drive\Unity Course\Squirrels\1495767615895765053_squirrels.zip" -F "kit[game_engine_version]=20192" https://account.altvr.com/api/kits/1495767615895765053.json
